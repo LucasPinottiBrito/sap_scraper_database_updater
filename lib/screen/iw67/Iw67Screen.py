ac70909caa6ffb9a7c61c892a0f5f4c5e1bbc7df
lucaspinottibrito <lucaspinottibrito@gmail.com>
2026-04-02 09:30:36 -0300

    fix/script_iw67

--- lib/screen/iw67/Iw67Screen.py
+++ lib/screen/iw67/Iw67Screen.py
@@ -24,6 +24,8 @@ class Iw67Screen(Iw67ScreenInterface):
             case "SC/RC":
                 if region == "SP":
                     self._session.findById("wnd[0]/tbar[1]/btn[17]").press()
+                    self._session.findById("wnd[1]/usr/txtV-LOW").setFocus()
+                    self._session.findById("wnd[1]/usr/txtV-LOW").caretPosition = 0
                     self._session.findById("wnd[1]/usr/txtV-LOW").text = "CONSIRREGULAR"
                     self._session.findById("wnd[1]/usr/txtENAME-LOW").text = ""
                     self._session.findById("wnd[1]/usr/txtENAME-LOW").setFocus()
